5948612063c3761bc9f82f09d8c6851e26430a39
HydraHash <117165597+HydraHash@users.noreply.github.com>
2023-01-11 22:25:45 +0100

    Daten in Array ablegen

--- Frontend/javascript/userRegistration.js
+++ Frontend/javascript/userRegistration.js
@@ -2,6 +2,9 @@
 
 function registerNewUser(){
     let maxId = 0;
+    let newUser = new Array();
+    let tableData = new Array();
+    let textFields = new Array();
     console.log('Get number of registered Users');
     $.ajax({
         url: 'http://localhost:8000/api/benutzer/alle',
@@ -17,16 +20,20 @@ function registerNewUser(){
             maxId++;
         });
 
-        let newUser = new Array();
-        let tableData = new Array();
-        let textFields = new Array();
         textFields = document.querySelectorAll("table tr");
+        let counter = 0;
         for(let i=0; i<textFields.length; i++){
             tableData = textFields[i].querySelectorAll(".input_txt");
             for(let j=0; j<tableData.length; j+=2){
-                console.log(tableData[j].value);
+                newUser[counter] = tableData[j].value;
+                counter++;
             }
         }
+
+        for(let k=0; k<newUser.length; k++){
+            console.log(k);
+            console.log(newUser[k]);
+        }        
 
         formOfAdress = document.querySelector('input[name=xor]:checked').value;
         if (formOfAdress == "w"){
@@ -37,4 +44,27 @@ function registerNewUser(){
     }).fail(function (){
         console.log('Problem while finding MaxID');
     });
+
+    console.log('Push Content to DB');
+    let newData = JSON.stringify({
+        'id': maxId,
+        'passwort': tableData[14]/*,
+        'benutzername': newValues[0],
+        'anrede': newValues[1],
+        'nachname': newValues[2],
+        'vorname': newValues[3],
+        'email': newValues[4],
+        'strasse': newValues[5],
+        'hausnummer': newValues[6],
+        'plz': newValues[7],
+        'ort': newValues[8],
+        'isAdmin': newValues[9]*/
+    })/*
+    $.ajax({
+        url: 'http://localhost:8000/api/benutzer',
+        method: 'put',
+        contentType: 'application/json; charset=utf-8',
+        cache: false,
+        data: newData,
+    })*/
 }
