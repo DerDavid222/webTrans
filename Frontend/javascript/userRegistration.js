f0a3fe3e1599e5a2d41223e3369940b015029355
HydraHash <117165597+HydraHash@users.noreply.github.com>
2023-01-17 13:30:28 +0100

    Registrierung um Hausnummer erweitern

--- Frontend/javascript/userRegistration.js
+++ Frontend/javascript/userRegistration.js
@@ -43,16 +43,16 @@ function registerNewUser(){
         dataType: 'json',
       }).done(function (outerResponse) {
         let newData = JSON.stringify({
-                'passwort': newUser[7],
+                'passwort': newUser[8],
                 'benutzername': newUser[0],
                 'anrede': newUser[9],
                 'nachname': newUser[1],
                 'vorname': newUser[2],
                 'email': newUser[3],
                 'strasse': newUser[4],
-                'hausnummer': '187',
-                'plz': newUser[5],
-                'ort': newUser[6],
+                'hausnummer': newUser[5],
+                'plz': newUser[6],
+                'ort': newUser[7],
                 'isAdmin': 0
               })
         $.ajax({
